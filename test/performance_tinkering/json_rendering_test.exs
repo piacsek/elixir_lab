a2427835788c3b6fb14ae00d3b5a405c1821b66a
piacsek <felipe@piacsek.me>
2025-11-01 14:26:02 -0700

    another 20k run

--- test/performance_tinkering/json_rendering_test.exs
+++ test/performance_tinkering/json_rendering_test.exs
@@ -11,8 +11,14 @@ end
 
 # Results breakdown
 # 20k tests: Finished in 444.6 seconds (444.6s async, 0.00s sync)
+# 20k tests: Finished in 486.1 seconds (486.1s async, 0.00s sync)
+
 # 2k tests: Finished in 13.1 seconds (13.1s async, 0.00s sync)
+
 # 200 tests: Finished in 1.6 seconds (1.6s async, 0.00s sync)
+
 # 20 tests: Finished in 0.1 seconds (0.1s async, 0.00s sync)
+
 # 2 tests: Finished in 0.04 seconds (0.04s async, 0.00s sync)
+
 # 1 test: Finished in 0.04 seconds (0.04s async, 0.00s sync)
